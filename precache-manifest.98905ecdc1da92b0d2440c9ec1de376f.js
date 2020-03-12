78ecdc36d08f484fa8e6010dbaded7060cd4a150
mdiedricks <michael.diedricks@gmail.com>
2020-03-12 22:24:36 +1100

    Updates

---
+++ precache-manifest.98905ecdc1da92b0d2440c9ec1de376f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20284d3408e87cb37a7119ec8ce2453b",
+    "url": "/mdiedricks/index.html"
+  },
+  {
+    "revision": "af3dc39fad9925735faa",
+    "url": "/mdiedricks/static/css/main.c6a4111e.chunk.css"
+  },
+  {
+    "revision": "ba270b41452110aff8c5",
+    "url": "/mdiedricks/static/js/2.89300d07.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/mdiedricks/static/js/2.89300d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3dc39fad9925735faa",
+    "url": "/mdiedricks/static/js/main.3697b2bb.chunk.js"
+  },
+  {
+    "revision": "e0896222d3cbc938d4e5",
+    "url": "/mdiedricks/static/js/runtime-main.1e8102f1.js"
+  },
+  {
+    "revision": "310769232d6effde45e3fa3c92367292",
+    "url": "/mdiedricks/static/media/mdiedricks_2020.31076923.pdf"
+  }
+]);
